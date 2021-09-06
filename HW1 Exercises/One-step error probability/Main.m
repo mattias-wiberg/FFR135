39b83ff1faeec1072dad615ec208517bfaf069b1
Mattias <klutten99@gmail.com>
2021-09-06 16:33:22 +0200

    Faster Version
    Only calculate the needed weights for the node to get next state of.

--- HW1 Exercises/One-step error probability/Main.m
+++ HW1 Exercises/One-step error probability/Main.m
@@ -10,15 +10,16 @@ p_i = 1
 for pSize = p
     for t = 1:TRIALS
         pPatterns = GetRandomPatterns(N, pSize);
-        weights = Hebbs(pPatterns);
-        weights(eye(size(weights))==1)=0; % Set diagonal to 0 modified hebbs
         
         % Feed one known pattern fed to the network which one dosn't matter
         % since it is random to begin with here I am always using the first
         % one (x).
         i = randi(N);
-        x = pPatterns(:,1); 
-        newX = Sgn(weights(i,:)*x);% Modified sign function so 0 gives 1
+        x = pPatterns(:,1);
+        
+        weights = pPatterns(i,:)*pPatterns'; % Only calculate the weights that are going to be used
+        weights(i) = 0; % w_ii = 0
+        newX = Sgn(weights*x);% Modified sign function so 0 gives 1
         
         errorP(p_i) = errorP(p_i) + (x(i,1) ~= newX); % Increase if bit changed
     end
@@ -30,5 +31,6 @@ end
 errorP
 errorP/TRIALS
 plot(p,errorP/TRIALS)
-% 12        24        48        70       100       120
-% 0.0370    1.1610    5.6580    9.3970   13.6150   15.7870 %
+% 12        24        48        70        100       120
+% 0.0005    0.0114    0.0552    0.0951    0.1363    0.1598      w_ii = 0
+% 
