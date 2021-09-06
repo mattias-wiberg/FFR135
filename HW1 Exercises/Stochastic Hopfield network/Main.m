2a0ec7a9a8103808a9a4f6e52e863854df9256e1
klutten99 <klutten99@gmail.com>
2021-09-06 20:26:36 +0200

    Stochastic Hopfield network
    Skeleton implemented not tested yet

--- HW1 Exercises/Stochastic Hopfield network/Main.m
+++ HW1 Exercises/Stochastic Hopfield network/Main.m
@@ -1,11 +1,27 @@
 clear
 clc
 
-p = 7
-N = 200
-T = 2*10^5
-EXPERIMENTS = 100
+EXPERIMENTS = 100;
+BETA = 2;
+prob = @(b) 1/(1+exp(-2*BETA*b));
+
+p = 7;
+N = 200;
+T = 2*10^5;
+m = zeros(EXPERIMENTS, 1);
 
 for experiment = 1:EXPERIMENTS
-    patterns = GetRandomPatterns(N, p)
+    patterns = GetRandomPatterns(N, p);
+    weights = Hebbs(patterns);
+    weights(eye(size(weights))==1) = 0; % Set diagonal to zeros w_ii=0
+    s = patterns(:,1); % feed _x^(1)
+    
+    for update = 1:T
+        i = randi(N);
+        s(i) = Sgn(weights(i,:)*s);
+        m(experiment) = m(experiment) + 1/N*s'*patterns(:,1);
+    end
+    m(experiment) = m(experiment)/T
 end
+
+sum(m)/EXPERIMENTS
