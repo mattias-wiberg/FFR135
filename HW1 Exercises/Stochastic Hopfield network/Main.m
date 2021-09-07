1273ed0b6c1f55dc2793c061a32dea13c2bb6c53
Mattias <klutten99@gmail.com>
2021-09-07 12:45:44 +0200

    Fixed probability
    State s' was wrongfully set now this has been fixed. Using prob as the probability function.

--- HW1 Exercises/Stochastic Hopfield network/Main.m
+++ HW1 Exercises/Stochastic Hopfield network/Main.m
@@ -5,7 +5,7 @@ EXPERIMENTS = 100;
 BETA = 2;
 prob = @(b) 1/(1+exp(-2*BETA*b));
 
-p = 7;
+p = 45;
 N = 200;
 T = 2*10^5;
 m = zeros(EXPERIMENTS, 1);
@@ -18,11 +18,22 @@ for experiment = 1:EXPERIMENTS
     
     for update = 1:T
         i = randi(N);
-        s(i) = Sgn(prob(weights(i,:)*s));
+        b_i = weights(i,:)*s;
+        if rand() < prob(b_i)
+            s(i) = 1;
+        else
+            s(i) = -1;
+        end
         m(experiment) = m(experiment) + 1/N*s'*patterns(:,1);
     end
     m(experiment) = m(experiment)/T;
-    m(experiment)
+    experiment
 end
 
-sum(m)/EXPERIMENTS
+avg = sum(m)/EXPERIMENTS
+plot(1:EXPERIMENTS, m)
+
+% <m_1(T)>  T=2⋅10^​5, p=7, N=200, β=2
+% 0.9020
+% <m_1(T)>  T=2⋅10^​5, p=45, N=200, β=2
+% 0.1422
